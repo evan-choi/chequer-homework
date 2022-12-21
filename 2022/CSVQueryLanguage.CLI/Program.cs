74586026304f72bfc38bbf687924c687ff53fb97
evan-choi <evan@chequer.io>
2022-12-21 22:22:05 +0900

    Setup project

---
+++ 2022/CSVQueryLanguage.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSVQueryLanguage.CLI;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
